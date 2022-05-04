8ed06e6f239b3105ff0e2c2330ef76f402f6be51
TuanLeAnh1003 <nhoxtboy1000@gmail.com>
2022-05-05 00:00:46 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -2,13 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import products from './api/products.route.js';
 import users from './api/users.route.js';
-<<<<<<< HEAD
 import orders from './api/orders.route.js';
 import carts from './api/carts.route.js';
 import ratings from './api/ratings.route.js';
-=======
 import news from './api/news.route.js';
->>>>>>> origin/Thien
 
 const app = express();
 
@@ -17,13 +14,10 @@ app.use(express.json());
 
 app.use("/api/v1/products", products);
 app.use("/api/v1/users", users);
-<<<<<<< HEAD
 app.use("/api/v1/orders", orders);
 app.use("/api/v1/carts", carts);
 app.use("/api/v1/ratings", ratings);
-=======
 app.use("/api/v1/news", news);
->>>>>>> origin/Thien
 app.use('*', (req, res) => {
     res.status(404).json({ error: "not found" })
 });
