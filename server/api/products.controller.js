f4c84199479664552cb76f9f02f00b98cbadf966
Nguyen Huy Khoa <19521697@gm.uit.edu.vn>
2022-05-25 11:26:54 +0700

    Done Search By Name (products/?name=)

--- server/api/products.controller.js
+++ server/api/products.controller.js
@@ -2,7 +2,7 @@ import ProductsDAO from '../dao/productsDAO.js';
 
 export default class ProductsController {
     static async apiGetProducts(req, res, next) {
-        const productsPerPage = req.query.productsPerPage ? parseInt(req.query.productsPerPage) : 20;
+        const productsPerPage = req.query.productsPerPage ? parseInt(req.query.productsPerPage) : 30;
         const page = req.query.page ? parseInt(req.query.page) : 0;
         let filters = {};
         if (req.query.name) {
@@ -20,46 +20,42 @@ export default class ProductsController {
         const productsPerPage = req.query.productsPerPage ? parseInt(req.query.productsPerPage) : 20;
         const page = req.query.page ? parseInt(req.query.page) : 0;
         let filters = {};
-        if (req.query.name) {
-            filters.name = req.query.name;
-        }
-        filters.type={};
         if (req.body.brand)
-            filters.type["type.brand"] = req.body.brand;
+            filters["type.brand"] = req.body.brand;
 
         if (req.body.albert)
-            filters.type["type.albert"] = req.body.albert;
+            filters["type.albert"] = req.body.albert;
 
         if (req.body.priceRange)
-            filters.type["type.priceRange"] = req.body.priceRange;
+            filters["type.priceRange"] = req.body.priceRange;
 
         if (req.body.glass)
-            filters.type["type.glass"] = req.body.glass;
+            filters["type.glass"] = req.body.glass;
 
         if (req.body.energyCore)
-            filters.type["type.energyCore"] = req.body.energyCore;
+            filters["type.energyCore"] = req.body.energyCore;
 
         if (req.body.interfaceColor)
-            filters.type["type.interfaceColor"] = req.body.interfaceColor;
+            filters["type.interfaceColor"] = req.body.interfaceColor;
 
         if (req.body.caseColor)
-            filters.type["type.caseColor"] = req.body.caseColor;
+            filters["type.caseColor"] = req.body.caseColor;
 
         if (req.body.shape)
-            filters.type["type.shape"] = req.body.shape;
+            filters["type.shape"] = req.body.shape;
 
         if (req.body.size)
-            filters.type["type.size"] = req.body.size;
+            filters["type.size"] = req.body.size;
 
         if (req.body.waterRessitance)
-            filters.type["type.waterRessitance"] = req.body.waterRessitance;
+            filters["type.waterRessitance"] = req.body.waterRessitance;
 
         if (req.body.feature)
-            filters.type["type.feature"] = req.body.feature;
+            filters["type.feature"] = req.body.feature;
 
-        if (Object.keys(filters.type).length === 0) delete filters.type;
+        if (Object.keys(filters).length === 0) delete filters.type;
 
-        const { productsList, totalNumProducts } = await ProductsDAO.getProducts({
+        const { productsList, totalNumProducts } = await ProductsDAO.getFilterProducts({
             filters, page,
             productsPerPage
         });
